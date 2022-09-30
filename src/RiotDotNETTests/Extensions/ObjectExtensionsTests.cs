f891501bcf1b3b3318c3de729b9d4fce7c4c4a93
EthanDelong <48994953+EthanDelong@users.noreply.github.com>
2022-09-30 01:19:06 -0400

    reformat unit tests again. issue with coverage looks like it was because none of the tests returned null. also added a few more tests for different types.

--- src/RiotDotNETTests/Extensions/ObjectExtensionsTests.cs
+++ src/RiotDotNETTests/Extensions/ObjectExtensionsTests.cs
@@ -1,24 +1,38 @@
 ﻿namespace RiotDotNET.Extensions.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiotDotNET.Enums;
 
 [TestClass]
 public class ObjectExtensionsTests
 {
     [TestMethod]
-    public void ToStringLowerTest()
-    {
-        Dictionary<object, string?> tests = new()
-        {
-            [""] = "",
-            [" Test .,! "] = " test .,! ",
-            ["TEST"] = "test",
-            [123] = "123",
-        };
-        foreach (var inputExpected in tests)
-        {
-            Assert.AreEqual(inputExpected.Value, inputExpected.Key.ToStringLower());
-        }
+    [DataRow("", "")]
+    [DataRow("TEST", "test")]
+    [DataRow(123, "123")]
+    [DataRow(" Test .,! ", " test .,! ")]
+    [DataRow(true, "true")]
+    [DataRow(false, "false")]
+    [DataRow(0.1519, "0.1519")]
+    [DataRow(PlatformRoute.BR1, "br1")]
+    [DataRow(PlatformRoute.EUN1, "eun1")]
+    [DataRow(PlatformRoute.EUW1, "euw1")]
+    [DataRow(PlatformRoute.JP1, "jp1")]
+    [DataRow(PlatformRoute.KR, "kr")]
+    [DataRow(PlatformRoute.LA1, "la1")]
+    [DataRow(PlatformRoute.LA2, "la2")]
+    [DataRow(PlatformRoute.NA1, "na1")]
+    [DataRow(PlatformRoute.OC1, "oc1")]
+    [DataRow(PlatformRoute.TR1, "tr1")]
+    [DataRow(PlatformRoute.RU, "ru")]
+    [DataRow(RegionRoute.AMERICAS, "americas")]
+    [DataRow(RegionRoute.ASIA, "asia")]
+    [DataRow(RegionRoute.EUROPE, "europe")]
+    [DataRow(RegionRoute.SEA, "sea")]
+    public void ToStringLowerBasicTest(object input, string expected) => Assert.AreEqual(expected, input.ToStringLower());
 
+    [TestMethod]
+    public void ToStringLowerAdvancedTest()
+    {
         Assert.AreEqual(" - (test): custom object! ", new CustomObject { Name = "tEsT", Value = "CuSTOM oBJEcT! " }.ToStringLower());
         Assert.AreEqual(null, new NullToString().ToStringLower());
 
