5db5645aa9f2632110be73bc763d81d478fcd0c2
EthanDelong <48994953+EthanDelong@users.noreply.github.com>
2022-09-30 00:39:02 -0400

    add platform and region tests

---
+++ src/RiotDotNETTests/Constants/PlatformTests.cs
@@ -0,0 +1,34 @@
+﻿namespace RiotDotNET.Constants.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiotDotNET.Constants;
+using RiotDotNET.Enums;
+
+[TestClass]
+public class PlatformTests
+{
+    [TestMethod]
+    public void PlatformIntegrity()
+    {
+        var expectedPlatforms = typeof(Platform).GetProperties().Where(x => x.PropertyType == typeof(Platform)).Select(x => x.GetValue(null, null)).ToArray();
+        CollectionAssert.AreEqual(expectedPlatforms, Platform.All);
+
+        foreach (var platform in Platform.All)
+        {
+            Assert.IsNotNull(platform.Name);
+            Assert.IsNotNull(platform.Route);
+            Assert.IsNotNull(platform.Code);
+            Assert.AreEqual(platform, Platform.FromCode(platform.Code));
+            Assert.AreEqual(platform, Platform.FromRoute(platform.Route));
+        }
+
+        foreach (var code in Enum.GetValues<PlatformCode>())
+        {
+            Assert.IsNotNull(Platform.FromCode(code));
+        }
+
+        foreach (var route in Enum.GetValues<PlatformRoute>())
+        {
+            Assert.IsNotNull(Platform.FromRoute(route));
+        }
+    }
+}
---
+++ src/RiotDotNETTests/Constants/RegionTests.cs
@@ -0,0 +1,27 @@
+﻿namespace RiotDotNET.Constants.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RiotDotNET.Constants;
+using RiotDotNET.Enums;
+
+[TestClass]
+public class RegionTests
+{
+    [TestMethod]
+    public void RegionIntegrity()
+    {
+        var expectedRegions = typeof(Region).GetProperties().Where(x => x.PropertyType == typeof(Region)).Select(x => x.GetValue(null, null)).ToArray();
+        CollectionAssert.AreEqual(expectedRegions, Region.All);
+
+        foreach (var region in Region.All)
+        {
+            Assert.IsNotNull(region.Name);
+            Assert.IsNotNull(region.Route);
+            Assert.AreEqual(region, Region.FromRoute(region.Route));
+        }
+
+        foreach (var route in Enum.GetValues<RegionRoute>())
+        {
+            Assert.IsNotNull(Region.FromRoute(route));
+        }
+    }
+}
