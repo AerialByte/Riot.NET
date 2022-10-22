4adae5494f7a8e10feff743f1f37896c5cdde556
EthanDelong <48994953+EthanDelong@users.noreply.github.com>
2022-10-22 11:49:05 -0400

    endpoint consts go until the version (for consistency across all coming endpoints)

--- src/RiotDotNETTests/TestHelpers/TestHttpMessageHandler.cs
+++ src/RiotDotNETTests/TestHelpers/TestHttpMessageHandler.cs
@@ -19,8 +19,8 @@ public class TestHttpMessageHandler : HttpMessageHandler
         [HttpStatusCode.NotFound] = @"{""status"":{""message"":""Data not found"",""status_code"":404}}",
     };
 
-    const string accountV1Endpoint = "/riot/account/v1/accounts";
-    const string summonerV4Endpoint = "/lol/summoner/v4/summoners";
+    const string accountV1Endpoint = "/riot/account/v1";
+    const string summonerV4Endpoint = "/lol/summoner/v4";
 
     private static readonly Dictionary<string, Dictionary<string, string>> responses = new()
     {
@@ -29,19 +29,19 @@ public class TestHttpMessageHandler : HttpMessageHandler
 
         [accountV1Endpoint] = new()
         {
-            ["/by-riot-id/Agurin/EUW"] = @"{
+            ["/accounts/by-riot-id/Agurin/EUW"] = @"{
     ""puuid"": ""UNyvfnTchNzhdHA9Y_PAegVcaUOy0BaGY60bhT9vVgOmoOohUkmEBywVZiMCuRrB_lN14JgjHBsgaw"",
     ""gameName"": ""Agurin"",
     ""tagLine"": ""EUW""
 }",
 
-            ["/by-riot-id/lanegg/NA1"] = @"{
+            ["/accounts/by-riot-id/lanegg/NA1"] = @"{
     ""puuid"": ""ykDllSHvCjx0dPvV_sQDBdgjoylk7OPGxPdt49pk6vox2nZ2AvRl9cGQHDKP6Uvtgm8J2KTZHxF4YQ"",
     ""gameName"": ""lanegg"",
     ""tagLine"": ""NA1""
 }",
 
-            ["/by-riot-id/COOKIEMONSTER123/NA1"] = @"{
+            ["/accounts/by-riot-id/COOKIEMONSTER123/NA1"] = @"{
     ""puuid"": ""q-59OC-fr19MM_ev8zX7SEkAe_5DK6xID7Lbri_51iqEKN5w1vbgrHXeuxPeBvg96A2XkpBPqZ-2TQ"",
     ""gameName"": ""COOKIEMONSTER123"",
     ""tagLine"": ""NA1""
@@ -54,7 +54,7 @@ public class TestHttpMessageHandler : HttpMessageHandler
 
         [summonerV4Endpoint] = new()
         {
-            ["/by-name/Agurin"] = @"{
+            ["/summoners/by-name/Agurin"] = @"{
     ""id"": ""oTWjkY-F-oLAqHs5qkM54UnvxS8cpAewKAv9swy7oKSzh0c"",
     ""accountId"": ""B54j00MSC0onav0-DCe6adKarIn6ALIHeyw9Bxv3CicTiQ"",
     ""puuid"": ""UNyvfnTchNzhdHA9Y_PAegVcaUOy0BaGY60bhT9vVgOmoOohUkmEBywVZiMCuRrB_lN14JgjHBsgaw"",
@@ -64,7 +64,7 @@ public class TestHttpMessageHandler : HttpMessageHandler
     ""summonerLevel"": 791
 }",
 
-            ["/by-name/lanegg"] = @"{
+            ["/summoners/by-name/lanegg"] = @"{
     ""id"": ""VN_zm246D0g0nbmtroeam98OhX8LwYLlrhy07FAjmCB3yUu3GIdN22QCCQ"",
     ""accountId"": ""yKz49zfgnkVTEYG7-rPJZr60B7p_-Ek5Tcjb1_4niaCEO_rDUwGqS8DW"",
     ""puuid"": ""ykDllSHvCjx0dPvV_sQDBdgjoylk7OPGxPdt49pk6vox2nZ2AvRl9cGQHDKP6Uvtgm8J2KTZHxF4YQ"",
@@ -74,7 +74,7 @@ public class TestHttpMessageHandler : HttpMessageHandler
     ""summonerLevel"": 29
 }",
 
-            ["/by-name/COOKIEMONSTER123"] = @"{
+            ["/summoners/by-name/COOKIEMONSTER123"] = @"{
     ""id"": ""4Vaug7PDl8Rt8nZxYrEciphqzMiUTai-q3SW6hzjGXi8VK4IulsiAIvQ8w"",
     ""accountId"": ""TBJXabWboLPseVmGoS6oyqs_TrqkwW3LNhMdj4z5-_a0_g-SpgM-IFEG"",
     ""puuid"": ""q-59OC-fr19MM_ev8zX7SEkAe_5DK6xID7Lbri_51iqEKN5w1vbgrHXeuxPeBvg96A2XkpBPqZ-2TQ"",
@@ -93,15 +93,15 @@ public class TestHttpMessageHandler : HttpMessageHandler
         foreach (string accountJson in responses[accountV1Endpoint].Values.ToArray())
         {
             var account = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(accountJson) ?? throw new Exception("Invalid test setup -- HttpMessageHandler json.");
-            responses[accountV1Endpoint][$"/by-puuid/{account["puuid"]}"] = accountJson;
+            responses[accountV1Endpoint][$"/accounts/by-puuid/{account["puuid"]}"] = accountJson;
         }
 
         foreach (string summonerJson in responses[summonerV4Endpoint].Values.ToArray())
         {
             var summoner = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(summonerJson) ?? throw new Exception("Invalid test setup -- HttpMessageHandler json.");
-            responses[summonerV4Endpoint][$"/{summoner["id"]}"] = summonerJson;
-            responses[summonerV4Endpoint][$"/by-account/{summoner["accountId"]}"] = summonerJson;
-            responses[summonerV4Endpoint][$"/by-puuid/{summoner["puuid"]}"] = summonerJson;
+            responses[summonerV4Endpoint][$"/summoners/{summoner["id"]}"] = summonerJson;
+            responses[summonerV4Endpoint][$"/summoners/by-account/{summoner["accountId"]}"] = summonerJson;
+            responses[summonerV4Endpoint][$"/summoners/by-puuid/{summoner["puuid"]}"] = summonerJson;
         }
     }
 
